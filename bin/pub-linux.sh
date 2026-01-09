e8a8e4fdffe82bc3e8251fb506c04ae7142c1524
larpoux <larpoux@gmail.com>
2026-01-09 22:49:28 +0100

    Scrogheugneu

--- bin/pub-linux.sh
+++ bin/pub-linux.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 rm -rf build .svelte-kit dist-electron
 BUILD_TARGET=desktop VITE_BUILD_TARGET=desktop npm run build:desktop
 # vérifiez que le log dit bien: "Using @sveltejs/adapter-static" + "Wrote site to "build""
 CSC_IDENTITY_AUTO_DISCOVERY=false npx electron-builder --mac --dir
 npx electron-builder --linux AppImage deb --x64
-
 
